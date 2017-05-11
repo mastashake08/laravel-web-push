0a2b37c62639f97a13798a3816fd74da50c1d7d4
Jyrone Parker <jyrone.parker@gmail.com>
2017-05-11 03:10:40 +0000

    notification logic

---
+++ app/Notifications/GenericNotification.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use NotificationChannels\WebPush\WebPushMessage;
+use NotificationChannels\WebPush\WebPushChannel;
+class GenericNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [WebPushChannel::class];
+    }
+
+    public function toWebPush($notifiable, $notification)
+    {
+      $time = \Carbon\Carbon::now();
+        return WebPushMessage::create()
+            // ->id($notification->id)
+            ->title('This Is A Random Webpush')
+            //->icon('/approved-icon.png')
+            ->body("The time is {$time}!")
+            //->action('View account', 'view_account');
+    }
+
+}
