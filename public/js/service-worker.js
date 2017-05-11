49dd5a6255ff3720ab0bf8da9c5e45a9034d0500
Jyrone Parker <jyrone.parker@gmail.com>
2017-05-11 03:50:12 +0000

    push png

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -2,7 +2,8 @@ self.addEventListener('push', function(event) {
   if (event.data) {
     var data = event.data.json();
     self.registration.showNotification(data.title,{
-      body: data.body
+      body: data.body,
+      icon: data.icon
     });
     console.log('This push event has data: ', event.data.text());
   } else {
