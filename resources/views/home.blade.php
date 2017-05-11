3d5065364b7785f4c1463ed7e40f7fceb05ddba1
Jyrone Parker <jyrone.parker@gmail.com>
2017-05-11 03:10:22 +0000

    client side registration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,107 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <button class="btn btn-info" id="enable-notifications" onclick="enableNotifications()"> Enable Push Notifications </button>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+function registerServiceWorker() {
+  return navigator.serviceWorker.register('service-worker.js')
+  .then(function(registration) {
+    console.log('Service worker successfully registered.');
+    return registration;
+  })
+  .catch(function(err) {
+    console.error('Unable to register service worker.', err);
+  });
+}
+function askPermission() {
+  return new Promise(function(resolve, reject) {
+    const permissionResult = Notification.requestPermission(function(result) {
+      resolve(result);
+    });
+
+    if (permissionResult) {
+      permissionResult.then(resolve, reject);
+    }
+  })
+  .then(function(permissionResult) {
+    if (permissionResult !== 'granted') {
+      throw new Error('We weren\'t granted permission.');
+    }
+    else{
+      subscribeUserToPush();
+    }
+  });
+}
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+function getSWRegistration(){
+  return navigator.serviceWorker.getRegistration();
+}
+function subscribeUserToPush() {
+  return getSWRegistration()
+  .then(function(registration) {
+    const subscribeOptions = {
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(
+        "{{env('VAPID_PUBLIC_KEY')}}"
+      )
+    };
+
+    return registration.pushManager.subscribe(subscribeOptions);
+  })
+  .then(function(pushSubscription) {
+    console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
+    sendSubscriptionToBackEnd(pushSubscription);
+    return pushSubscription;
+  });
+}
+
+function sendSubscriptionToBackEnd(subscription) {
+  return fetch('/api/save-subscription/{{Auth::user()->id}}', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(subscription)
+  })
+  .then(function(response) {
+    if (!response.ok) {
+      throw new Error('Bad status code from server.');
+    }
+
+    return response.json();
+  })
+  .then(function(responseData) {
+    if (!(responseData.data && responseData.data.success)) {
+      throw new Error('Bad response from server.');
+    }
+  });
+}
+
+
+function enableNotifications(){
+  //register service worker
+  //check permission for notification/ask
+  askPermission();
+}
+registerServiceWorker();
+</script>
 @endsection
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,13 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/save-subscription/{id}',function($id, Request $request){
+  $user = \App\User::findOrFail($id);
+
+  $user->updatePushSubscription($request->input('endpoint'), $request->input('keys.p256dh'), $request->input('keys.auth'));
+  $user->notify(new \App\Notifications\GenericNotification());
+  return response()->json([
+    'success' => true
+  ]);
+});
