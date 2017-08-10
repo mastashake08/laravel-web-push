c3243431ce7f2d8f9b84cc39298a2c87b5a53fef
Jyrone Parker <jyrone.parker@gmail.com>
2017-08-10 11:59:55 -0400

    web push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    {{config('app.name')}}
+                    Web Push
 
                 </div>
                 <p>
