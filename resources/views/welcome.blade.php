9be0a2cb50e609c5c21ebbc97bc3ae3dfb447f30
Jyrone Parker <jyrone.parker@gmail.com>
2017-08-10 11:55:24 -0400

    Front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{config('app.name')}}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,15 +79,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{config('app.name')}}
+                    Simple web push example using Laravel 5.4 notifications.
+                    Register, enable notifications, have fun! Check out some of my
+                    other projects below!
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://jyroneparker.com" target="_blank">My Blog</a>
+                    <a href="https://treatmeathome.online" target="_blank">Treat Me At Home</a>
+                    <a href="https://anonvideochat.space" target="_blank">Anon Video Chat</a>
                 </div>
             </div>
         </div>
